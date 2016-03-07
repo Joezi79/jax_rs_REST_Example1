25b90ff296349f3d44bec6608ee80f7f19cc8117
sanketsw <sanketsw@au1.ibm.com>
2016-03-08 10:17:58 +1100

    JSON integration

--- src/main/java/com/ibm/api/HelloWorldExample.java
+++ src/main/java/com/ibm/api/HelloWorldExample.java
@@ -1,9 +1,16 @@
 package com.ibm.api;
 
+import java.io.Serializable;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.sun.jersey.core.header.MediaTypes;
+import com.sun.jersey.core.impl.provider.header.MediaTypeProvider;
+
  
 @Path("/hello")
 public class HelloWorldExample {
@@ -11,17 +18,38 @@ public class HelloWorldExample {
 	/**
 	 * Maps to REST API http://localhost:8080/jax_rs/rest/hello/sanket
 	 * @param msg "sanket"
-	 * @return "Jax-rs REST API : sanket"
+	 * @return JSON object with "Jax-rs REST API : sanket"
 	 * @see http://www.mkyong.com/webservices/jax-rs/jersey-hello-world-example/
 	 */
 	@GET
-	@Path("/{param}")
-	public Response getMsg(@PathParam("param") String msg) {
- 
+	@Path("/{param}")	
+	@Produces(MediaType.APPLICATION_JSON)
+	public Person getMsg(@PathParam("param") String msg) {
 		String output = "Jax-rs REST API : " + msg;
- 
-		return Response.status(200).entity(output).build();
- 
+		Person p = new Person();
+		p.name = "Jax-rs REST API";
+		p.lastname = output; 
+		//return Response.status(200).entity(output).build();
+		return p;
+	}
+	
+	class Person implements Serializable {
+		String name;
+		String lastname;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getLastname() {
+			return lastname;
+		}
+		public void setLastname(String lastname) {
+			this.lastname = lastname;
+		}
+		
+		
 	}
  
 }
