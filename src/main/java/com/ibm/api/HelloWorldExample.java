3ebe3079dfc229ea9cf982027e44f64095c29667
root <root@localhost>
2016-03-10 11:12:19 +1100

    Swagger integration

--- src/main/java/com/ibm/api/HelloWorldExample.java
+++ src/main/java/com/ibm/api/HelloWorldExample.java
@@ -16,67 +16,25 @@ import io.swagger.annotations.ApiResponses;
 
  
 @Path("/hello")
-@Api(value="/hello")
+@Api
 public class HelloWorldExample {
  
-	/**
-	 * Maps to REST API http://localhost:8080/jax_rs/rest/hello/sanket
-	 * @param msg "sanket"
-	 * @return JSON object with "Jax-rs REST API : sanket"
-	 * @see http://www.mkyong.com/webservices/jax-rs/jersey-hello-world-example/
-	 */
-	@GET
-	@Path("/{param}")	
-	@Produces(MediaType.APPLICATION_JSON)
-	@ApiOperation(value = "say hello world", notes ="nothing else")
-	@ApiResponses(value={
-			@ApiResponse(code=200, message="OK"),
-			@ApiResponse(code=500, message="Something went wrong"),
-	})
-	public Person getMsg(@PathParam("param") String msg) {
-		String output = "Jax-rs REST API : " + msg;
-		Person p = new Person();
-		p.name = "Jax-rs REST API";
-		p.lastname = output; 
-		//return Response.status(200).entity(output).build();
-		return p;
-	}
+
 	
 	@GET
 	@Path("/")	
-	@Produces(MediaType.APPLICATION_JSON)
 	@ApiOperation(value = "say hello world, sanket", notes ="nothing else than sanket")
+	@Produces(MediaType.TEXT_PLAIN)
 	@ApiResponses(value={
 			@ApiResponse(code=200, message="OK"),
 			@ApiResponse(code=500, message="Something went wrong"),
 	})
-	public Response getMsg() {
+	public String getMsg1() {
 		
-		return Response.status(200).entity("Hellow world").build();
+		return "Hello World from " + Result.IMPLEMENTATION;
 		
 	}
 	
-	class Person implements Serializable {
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 3955074699838807226L;
-		String name;
-		String lastname;
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public String getLastname() {
-			return lastname;
-		}
-		public void setLastname(String lastname) {
-			this.lastname = lastname;
-		}
-		
-		
-	}
+
  
 }
